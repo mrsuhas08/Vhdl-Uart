03dd6dd8048ea368cda354be66f1447af3970d64
mrsuhas08 <96473622+mrsuhas08@users.noreply.github.com>
2026-01-13 14:44:26 +0530

    Update Baud Rate.vhd

--- Baud Rate.vhd
+++ Baud Rate.vhd
@@ -1,9 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.12.2025 10:12:26
+-- Design Name: 
+-- Module Name: Baud_rate - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity Baud_rate is
     Port(clk    :   in std_logic;
          rst    :   in std_logic;
          
-         tx_en  :   out std_logic;
-         rx_en  :   out std_logic);
+         tx_en_b:   out std_logic;
+         rx_en_b:   out std_logic);
 end Baud_rate;
 
 architecture Behavioral of Baud_rate is
@@ -18,7 +52,7 @@ begin
             
         if rising_edge(clk) then
         
-            if rst ='1' then
+            if rst ='0' then
                 tx_count    <=  (others => '0');
                 rx_count    <=  (others => '0');
             
@@ -41,7 +75,7 @@ begin
     
     end process;
     
-    tx_en   <=  '1'     when tx_count   =   0   else    '0';
-    rx_en   <=  '1'     when rx_count   =   0   else    '0';
+    tx_en_b <=  '1'     when tx_count   =   0   else    '0';
+    rx_en_b <=  '1'     when rx_count   =   0   else    '0';
     
 end Behavioral;
